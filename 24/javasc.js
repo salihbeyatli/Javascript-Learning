b7d6b1905128afe526142ea0329360760006ca3d
Salih Beyatli <salihbeyatli@gmail.com>
2022-09-03 14:57:43 +0300

    24. practice

---
+++ 24/javasc.js
@@ -0,0 +1,54 @@
+// FORMLARLA ÇALIŞMAK: BÖLÜM SONU EGZERSİZİ
+
+/*
+1: Formu seç
+2: Input bilgisini ul içerisine ekle
+3: form içindeki bilgiyi sıfırla
+4: eğer forma bilgi girilmezse kullanıcıyı uyar
+*/
+
+let userFormDOM = document.querySelector("#userForm")
+userFormDOM.addEventListener("submit", formHandler)
+const alertDOM = document.querySelector("#alert")
+
+const alertFunction = (title, message, className="warning") => `
+<div class="alert alert-${className} alert-dismissible fade show" role="alert">
+  <strong>${title}</strong> ${message}
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+`
+
+function formHandler(event){
+    event.preventDefault()
+    const USER_NAME = document.querySelector("#username")
+    const SCORE = document.querySelector("#score")
+    if (USER_NAME.value && SCORE.value){
+        addItem(USER_NAME.ariaValueMax, SCORE.value) // gönderdikten sonra sıfırlama
+        USER_NAME.value = ""
+        SCORE.value = ""
+    } else {
+        console.log("hatalı giriş")
+        alertDOM.innerHTML = alertFunction(
+            "Başlık bilgisi",
+            "Eksik Bilgi Girdiniz",
+            "danger"
+            )
+    }
+    
+}
+
+/*<li class="list-group-item d-flex justify-content-between align-items-center">
+    <span class="badge bg-primary rounded-pill">14</span>
+</li>*/
+
+
+let userListDOM = document.querySelector("#userList")
+const addItem = (userName, score) => {
+    let liDOM = document.createElement("li")
+    liDOM.innerHTML = `${userName}<span class="badge bg-primary rounded-pill">${score}</span>`
+    liDOM.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center")
+    userListDOM.append(liDOM)
+}
+
+
+
